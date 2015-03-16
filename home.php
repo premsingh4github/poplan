bd254be8ad49ac2522223ab49c79383dacdae223
premsingh4github <prem_aks@yahoo.com>
2015-03-16 17:57:14 +0545

    first day

---
+++ home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<title>this is html5</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet" />
+		<link rel="stylesheet" type="text/css" href="css/custom.css"/>
+		<script type="text/javascript" src="js/respond.js"></script>
+	</head>
+	<body>
+		this is body
+	</body>
+</html>
+
+<!-- <!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+</head>
+
+<body>
+Content of the document......
+</body>
+
+</html> -->
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+
+<html>
+
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<title>bootstrap test</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet" />
+		<link rel="stylesheet" type="text/css" href="css/custom.css"/>
+		<script type="text/javascript"></script>
+
+	</head>
+	
+	<body> 
+		<div class="header1">
+			djfkdjfkdfkdjfkdj
+		</div>
+		<div class="container">
+			<header  class="row">
+				jdhfjsdjfshjdsdsfsdf
+			</header>
+			<footer class="row">
+				sfksjdkjsdfsfdsdfsdfsfsdfsfsfdsdfsdfkjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+			</footer>
+		</div>
+		
+	<!--  javascript -->
+		<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	</body>
+</html>
