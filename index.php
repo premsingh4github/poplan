d01344a5d3964903b88a3fa3925f013487d3245d
premsingh4github <prem_aks@yahoo.com>
2015-03-17 17:40:54 +0545

    secend day

--- index.php
+++ index.php
@@ -14,14 +14,112 @@
 	
 	<body> 
 		<div class="header1">
-			djfkdjfkdfkdjfkdj
+			<div class="row">				
+				<div class="col-xs-12 col-sm-5">
+					<img class="logo" src="images/logo.gif">
+				</div>
+				<div class="col-xs-12 col-sm-5 search">
+					<input  name="search_text" type="text" class="text-box col-sm-6" value="  "/><img class="searchbtn" id="searchSubmit" class="search-img" src="images/search-bt1.gif">
+					<div class="col-xs-12  center">
+						<div class="menu">
+						                                        
+                            <ul>
+                                <li class="item-592">
+                                    <a style="padding: 12px 20px;" href="/inicio.html">Artículos</a>
+                                    <div class="submenu">
+                                        <div class="submenuTitle">
+                                            <span style="margin-left: 30px;">Recetario</span>
+                                            <span ><a href="/recetario/desayunos.html">Desayunos</a></span>
+                                            <span><a href="/recetario/desayunos.html"> Comidas</a></span>
+                                            <span><a href="/recetario/desayunos.html">Cenas</a></span>
+                                            <span><a href="/recetario/desayunos.html">Postres</a></span>
+                                            <span><a href="/recetario/desayunos.html">Botanas</a></span>
+                                        </div>
+                                        <div class="submenuTitle">
+                                            <span style="margin-left: 30px;" >Salud</span>
+                                            <span><a href="/recetario/desayunos.html">Alimentación</a></span>
+                                            <span><a href="/recetario/desayunos.html">Familia</a></span>
+                                            <span><a href="/recetario/desayunos.html">Embarazo</a></span>
+                                            <span><a href="/recetario/desayunos.html">Infografías</a></span>
+                                            <span><a href="/recetario/desayunos.html">Remedios de la Abuela</a></span>
+                                            <span><a href="/recetario/desayunos.html">Terapias</a></span>
+                                        </div>
+                                        <div class="submenuTitle">
+                                            <span style="margin-left: 30px;">Reciclando</span>
+                                            <span><a href="/recetario/desayunos.html">Decoración</a></span>
+                                            <span><a href="/recetario/desayunos.html"> Manualidades</a></span>
+                                        </div>
+                                        <div class="submenuTitle">
+                                            <span style="margin-left: 30px;">Estilo de Vida</span>
+                                            <span><a href="/recetario/desayunos.html">Anímate</a></span>
+                                            <span><a href="/recetario/desayunos.html">Celebrando</a></span>
+                                            <span><a href="/recetario/desayunos.html">Curiosidades</a></span><br>
+                                            <span><a href="/recetario/desayunos.html">Hogar</a></span><br><br><br>
+                                            <span><a href="/recetario/desayunos.html">Seguridad</a></span>
+                                        </div>
+                                        <div class="submenuTitle">
+                                            <span style="margin-left: 30px;">Literatura</span>
+                                            <span><a href="/recetario/desayunos.html">Infantil</a></span><br>
+                                            <span><a href="/recetario/desayunos.html">Novelas</a></span>
+                                            <span><a href="/recetario/desayunos.html">Psicología</a></span>
+                                        </div>
+                                        <div class="submenuTitle">
+                                            <span style="margin-left: 30px;">Tecnología</span>
+                                            <span><a href="/recetario/desayunos.html">Inventos</a></span>
+                                            <span><a href="/recetario/desayunos.html">Social Media</a></span>
+                                            <span><a href="/recetario/desayunos.html">Gadgets</a></span>
+                                            
+                                        </div>
+                                    </div>
+
+                                </li>
+                            </ul>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li class="item-592">
+                                    <a style="padding: 12px 20px;" href="/inicio.html">Cupones</a>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li class="item-592">
+                                    <a style="padding: 12px 20px;" href="/inicio.html">Directorio</a>
+                                </li>
+                            </ul>
+                            <ul>
+                                    <li class="item-592">
+                                        <a style="padding: 12px 20px;" href="/inicio.html">Inicio</a>
+                                    </li>
+                                </ul>
+						                                            
+						                                        
+						                                        
+						                                        
+						                                        
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-2 center">
+					<div class="contacto-login">
+                    	<div class="login-icon">
+                    		 <div class="loginimg"><img src="images/login-icon.gif"  alt=""></div>
+                    		<div class="logintext"><a  href="http://www.planblaguia.com/iniciar-sesion.html"> Login</a> </div> 
+                    	</div>
+                        <div class="login-icon ">
+                        	<div class="loginimg"><img src="images/contact-icon.gif" class="contact-icon" alt=""></div>
+                        	<div class="logintext"><a class="logintext" href="http://www.planblaguia.com/component/content/article.html?id=80"> Contacto</a></div>
+                        	
+                        </div>                                         
+                    </div>
+				</div>
+			</div>			
 		</div>
 		<div class="container">
 			<header  class="row">
-				jdhfjsdjfshjdsdsfsdf
+				
 			</header>
 			<footer class="row">
-				sfksjdkjsdfsfdsdfsdfsfsdfsfsfdsdfsdfkjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+				
 			</footer>
 		</div>
 		
